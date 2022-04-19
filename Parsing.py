10a8d07ad7aa3081aa31ae8d2e5ed6e4521ad575
eliserust <eliserust@gmail.com>
2022-04-19 14:31:59 -0400

    updated util

--- Parsing.py
+++ Parsing.py
@@ -15,14 +15,25 @@ def parse_tsv(data_file):
     speakers = []
     parties = []
 
-    with open(data_file) as dd:
-        data = pd.read_csv(dd, sep='\t')
-        # Append data to individual lists
-        labels.append(data[data.columns[1]])
-        texts.append(data[data.columns[2]])
-        subjects.append(data[data.columns[3]])
-        speakers.append(data[data.columns[4]])
-        parties.append(data[data.columns[7]])
+    with open(data_file, 'r') as dd:
+        for line in dd:
+            fields = line.strip().split("\t")
+            labels.append(fields[1])
+            texts.append(fields[2])
+            subjects.append(fields[3])
+            speakers.append(fields[4])
+            parties.append(fields[7])
+
     return labels, texts, subjects, speakers, parties
+
+    #with open(data_file) as dd:
+    #    data = pd.read_csv(dd, sep='\t')
+    #    # Append data to individual lists
+    #    labels.append(data[data.columns[1]])
+    #    texts.append(data[data.columns[2]])
+    #    subjects.append(data[data.columns[3]])
+    #    speakers.append(data[data.columns[4]])
+    #    parties.append(data[data.columns[7]])
+    #return labels, texts, subjects, speakers, parties
 
 #labels, texts, subjects, speakers, parties = parse_tsv('liar_dataset/train.tsv')
--- fraud_text.py
+++ fraud_text.py
@@ -3,24 +3,52 @@ import numpy as np
 import pandas as pd
 import string
 import sys
+from sklearn.model_selection import train_test_split
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.stats import pearsonr
+from Parsing import parse_tsv
 
 # Text pre-processing
-
+def preprocess_text(text):
+    # CREDIT: Copied from sts_tfidf lab
+    """Preprocess one sentence:
+    tokenizes, lowercases, applies the Porter stemmer,
+     removes punctuation tokens and stopwords.
+     Returns a string of tokens joined by whitespace.
+     """
+    # Stemmer
+    stemmer = PorterStemmer()
+    stops = set(stopwords.words('english')) # Identify  stop words
+    toks = word_tokenize(text) # Tokenize sentence
+    toks_stemmed = [stemmer.stem(tok.lower()) for tok in toks] # STEM tokens
+    toks_nopunc = [tok for tok in toks_stemmed if tok not in string.punctuation] # Remove punctuation
+    toks_nostop = [tok for tok in toks_nopunc if tok not in stops] # Remove stopwords
+    return " ".join(toks_nostop)
 
 # Main function
-def main(datafile):
-    pass
+def main(train_data, test_data):
 
     # Load texts and labels
+    train_labels, train_texts, train_subjects, train_speakers, train_parties = parse_tsv(train_data)
+    test_labels, test_texts, test_subjects, test_speakers, test_parties = parse_tsv(test_data)
 
-    # Build TFIDF vector feature matrix
+    # Preprocess texts
+    #preproc_texts = [preprocess_text(text) for text in train_texts]
+
+    # Build TFIDF vector feature matrix and fit to data
+    train_texts = list(train_texts)
+    #print(train_texts)
+    #sys.exit()
+
+    vectorizer = TfidfVectorizer(input = "content", lowercase = True, analyzer = "word", use_idf = True, min_df = 10)
+    tfidf_vector = vectorizer.fit(train_texts)
+    print("Checking the vocabulary: ")
+    print(tfidf_vector.get_feature_names())
 
     # Load true/false labels into vector y, mapped to 0 and 1
-
-    # Shuffle, split, assign 75% to train, 25% to test
 
     # Train an SVM model
 
@@ -37,12 +65,12 @@ def main(datafile):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FraudulentTextDetection_Final')
-    parser.add_argument('--path', type=str, default="train.tsv",
+    parser.add_argument('--train', type=str, default="liar_dataset/train.tsv",
                         help='path to training set')
-    parser.add_argument('--function_words_path', type=str, default="ewl_function_words.txt",
-                        help='path to the list of words to use as features')
-    parser.add_argument('--seed', type=int, default=7,
-                        help='random seed for dataset split')
+    parser.add_argument('--dev', type=str, default="liar_dataset/valid.tsv",
+                        help='path to dev set')
+    #parser.add_argument('--seed', type=int, default=7,
+    #                    help='random seed for dataset split')
     args = parser.parse_args()
 
-    main(args.path, args.function_words_path, args.seed)
+    main(args.train, args.dev)
