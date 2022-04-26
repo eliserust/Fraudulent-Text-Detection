155e41b17478bf432cdf2d8a1580fc47a5bbbb5c
eliserust <eliserust@gmail.com>
2022-04-26 15:49:28 -0400

    move preprocessing model to separate file

--- preprocessing_fraud_text.py
+++ preprocessing_fraud_text.py
@@ -19,6 +19,27 @@ from sklearn import svm, datasets # New package
 
 warnings.filterwarnings("ignore") # ignore warnings
 
+# Text pre-processing
+def preprocess_text(text):
+    # CREDIT: Copied from sts_tfidf lab
+    """Preprocess one sentence:
+    tokenizes, lowercases, applies the Porter stemmer,
+     removes punctuation tokens and stopwords.
+     Returns a string of tokens joined by whitespace.
+     """
+    # Stemmer
+    stemmer = PorterStemmer()
+    stops = set(stopwords.words('english')) # Identify  stop words
+    toks = word_tokenize(text) # Tokenize sentence
+    toks_stemmed = [stemmer.stem(tok.lower()) for tok in toks] # STEM tokens
+    toks_nopunc = [tok for tok in toks_stemmed if tok not in string.punctuation] # Remove punctuation
+    toks_nostop = [tok for tok in toks_nopunc if tok not in stops] # Remove stopwords
+    # Replace numbers with #
+    for tok in toks_nostop:
+        if type(tok) == int:
+            tok = '#'
+    return " ".join(toks_nostop)
+
 # Main function
 def main(train_data, dev_data):
 
@@ -26,21 +47,27 @@ def main(train_data, dev_data):
     train_labels, train_texts, train_subjects, train_speakers, train_parties = parse_tsv(train_data)
     dev_labels, dev_texts, dev_subjects, dev_speakers, dev_parties = parse_tsv(dev_data)
 
-    # Build TFIDF vector feature matrix and fit to training data
-    vectorizer = TfidfVectorizer(input = "content", lowercase = True, analyzer = "word", min_df = 5)
-    tfidf_vector = vectorizer.fit_transform(train_texts) # Ik we're never to .fit_transform not sure what the workaround is
-    train_df = pd.DataFrame(tfidf_vector.toarray(), columns=vectorizer.get_feature_names())
+    # Preprocess texts
+    preproc_train_texts = [preprocess_text(text) for text in train_texts]
+    preproc_dev_texts = [preprocess_text(text) for text in dev_texts]
+
+
+    # TFIDF vector feature matrix for preprocessed data
+    preproc_vectorizer = TfidfVectorizer(input = "content", lowercase = True, analyzer = "word", use_idf = True,
+                                         min_df = 5, token_pattern = "\S+", ngram_range = (1,2))
+    tfidf_preproc = preproc_vectorizer.fit_transform(preproc_train_texts)
+    preproc_train_df = pd.DataFrame(tfidf_preproc.toarray(), columns=preproc_vectorizer.get_feature_names())
 
     # TFIDF vector feature matrix for dev data
-    dev_vector = vectorizer.transform(dev_texts)
-    dev_df = pd.DataFrame(dev_vector.toarray(), columns=vectorizer.get_feature_names())
+    dev_vector = preproc_vectorizer.transform(dev_texts)
+    dev_df = pd.DataFrame(dev_vector.toarray(), columns=preproc_vectorizer.get_feature_names())
 
-    print(train_df)
+    print(preproc_train_df)
     print(dev_df)
     print(train_labels)
 
 
-    print(f"The training data has shape {train_df.shape} and dtype {type(train_df)}")
+    print(f"The training data has shape {preproc_train_df.shape} and dtype {type(preproc_train_df)}")
     print(f"The testing data has shape {dev_df.shape} and dtype {type(dev_df)}")
     print(f"The training labels have shape {np.shape(train_labels)} and dtype {type(train_labels)}")
     print(f"The testing labels has shape {np.shape(dev_labels)} and dtype {type(dev_labels)}")
@@ -48,7 +75,7 @@ def main(train_data, dev_data):
 
     # Train an SVM model - linear kernel
     SVM_Model = LinearSVC(C = 1)  # Initialize SVM
-    SVM_Model.fit(train_df, train_labels)  # Train SVM with Training Data
+    SVM_Model.fit(preproc_train_df, train_labels)  # Train SVM with Training Data
 
     # Results
     print("Linear Kernel SVM prediction:\n", SVM_Model.predict(dev_df))
@@ -68,7 +95,7 @@ def main(train_data, dev_data):
 
     # Train SVM model - polynomial kernel
     SVM_poly = svm.SVC(kernel='poly', degree=3, C=1, decision_function_shape='ovo')
-    SVM_poly.fit(train_df, train_labels)
+    SVM_poly.fit(preproc_train_df, train_labels)
 
     # Results
     print("SVM polynomial kernel prediction:\n", SVM_poly.predict(dev_df))
@@ -88,7 +115,7 @@ def main(train_data, dev_data):
 
     # Train a multinomial NB model
     NB_Model = MultinomialNB()
-    NB_Model.fit(train_df, train_labels)
+    NB_Model.fit(preproc_train_df, train_labels)
     # Confusion Matrix
     NB_CM = confusion_matrix(dev_labels, NB_Model.predict(dev_df))
     print("\nThe confusion matrix for Multinomial NB is:")
