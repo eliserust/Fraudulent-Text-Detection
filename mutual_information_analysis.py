ee2329b7fa0125dd5f78b1b2aa8095a4ffae8b33
eliserust <eliserust@gmail.com>
2022-04-28 01:29:32 -0400

    finalizing mutual information analysis

--- mutual_information_analysis.py
+++ mutual_information_analysis.py
@@ -55,7 +55,7 @@ def main(train_data):
     preproc_train_df = pd.DataFrame(tfidf_preproc.toarray(), columns=preproc_vectorizer.get_feature_names())
 
 
-    #### Add in External Data (speakers, subjects, parties) to train
+    #### Vectorize subjects, speakers, parties data
     count_vec = CountVectorizer()
 
     train_speakers_vec = count_vec.fit_transform(train_speakers).toarray()
@@ -63,37 +63,23 @@ def main(train_data):
     speakers_df.columns = count_vec.get_feature_names()
 
 
-    train_subjects_vec = count_vec.transform(train_subjects).toarray()
+    train_subjects_vec = count_vec.fit_transform(train_subjects).toarray()
     subjects_df = pd.DataFrame(train_subjects_vec)
     subjects_df.columns = count_vec.get_feature_names()
 
-    train_parties_vec = count_vec.transform(train_parties).toarray()
+    train_parties_vec = count_vec.fit_transform(train_parties).toarray()
     parties_df = pd.DataFrame(train_parties_vec)
     parties_df.columns = count_vec.get_feature_names()
 
 
     ###### Mutual information
-    # Text
-    text_mi_score = MIC(preproc_train_df, train_labels)
-    indices = np.argpartition(text_mi_score, -10)[-10:].tolist()
-    top_texts = preproc_train_df.iloc[:, indices]
-    text_features = top_texts.columns.values.tolist()
-    print(text_features)
-
-    #Subjects
-    subjects_mi_score = MIC(subjects_df, train_labels)
-    indices = np.argpartition(subjects_mi_score, -10)[-10:].tolist()
-    top_texts = subjects_df.iloc[:, indices]
-    text_features = top_texts.columns.values.tolist()
-    print(text_features)
-    sys.exit()
-
     # Speakers
     speakers_mi_score = MIC(speakers_df, train_labels)
     indices = np.argpartition(speakers_mi_score, -10)[-10:].tolist()
     top_texts = speakers_df.iloc[:, indices]
     text_features = top_texts.columns.values.tolist()
     print(text_features)
+    print(top_texts)
 
     # Parties
     parties_mi_score = MIC(parties_df, train_labels)
@@ -101,6 +87,23 @@ def main(train_data):
     top_texts = parties_df.iloc[:, indices]
     text_features = top_texts.columns.values.tolist()
     print(text_features)
+    print(top_texts)
+
+    # Text
+    text_mi_score = MIC(preproc_train_df, train_labels)
+    indices = np.argpartition(text_mi_score, -10)[-10:].tolist()
+    top_texts = preproc_train_df.iloc[:, indices]
+    text_features = top_texts.columns.values.tolist()
+    print(text_features)
+    print(top_texts)
+
+    # Subjects
+    subjects_mi_score = MIC(subjects_df, train_labels)
+    indices = np.argpartition(subjects_mi_score, -10)[-10:].tolist()
+    top_texts = subjects_df.iloc[:, indices]
+    text_features = top_texts.columns.values.tolist()
+    print(text_features)
+    print(top_texts)
 
 
 if __name__ == '__main__':
